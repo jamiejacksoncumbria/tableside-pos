b9dce3a438461fe529409e5292e6a4d8a8661331
Jamie Jackson <jamiej@gopcpitstop.co.uk>
2026-09-20 21:18:32 +0300

    fix: make compact POS widget test deterministic

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,15 +7,20 @@ void main() {
   testWidgets('compact POS menu hides the shell until tables is selected', (
     tester,
   ) async {
+    tester.view.physicalSize = const Size(600, 800);
+    tester.view.devicePixelRatio = 1;
+    addTearDown(tester.view.resetPhysicalSize);
+    addTearDown(tester.view.resetDevicePixelRatio);
+
     await tester.pumpWidget(const ProviderScope(child: TableSideCYApp()));
     await tester.pumpAndSettle();
 
     expect(find.text('New order'), findsOneWidget);
-    expect(find.text('TableSide Hospitality'), findsNothing);
+    expect(find.text('TableSideCY Hospitality'), findsNothing);
 
     await tester.tap(find.byType(Tab).first);
     await tester.pumpAndSettle();
-    expect(find.text('TableSide Hospitality'), findsOneWidget);
+    expect(find.text('TableSideCY Hospitality'), findsOneWidget);
     expect(find.text('Tables & tabs'), findsOneWidget);
   });
 }
